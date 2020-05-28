46a3b93a131f79131183606116132d31199cb7d9
YEvhen-SHmatko <yevhen.shmatko@gmail.com>
2020-05-28 20:39:24 +0300

    create branch animation/bubble

---
+++ src/components/Animation/BG/Bubble/index.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+
+const Bubble = () => {
+  return (
+    <>
+      <canvas />
+    </>
+  );
+};
+export default Bubble;
--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import Bubble from '../components/Animation/BG/Bubble';
 
-const HomePage = () => <div>Test</div>;
+const HomePage = () => <Bubble width={500} height={250} ammount={40} />;
 export default HomePage;
